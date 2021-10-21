1a38bad34ffe2c2317e304c7a0004b415830451b
jian zou <jian.zou@shopee.com>
2021-10-21 17:56:47 +0800

    modify DAO method

--- store/mysql/job.go
+++ store/mysql/job.go
@@ -72,7 +72,7 @@ func (ms *MySQLStore) UpdateJobRun(jobRun *common.JobRun) error {
 func (ms *MySQLStore) GetJobRunsByPistageRunId(pistageRunId string) (jobRuns []common.JobRun, err error) {
 	var runModels []JobRunModel
 	fmt.Println("runModels 74")
-	err = ms.db.Debug().Find(runModels).Where("pistage_run_id = ? ", pistageRunId).Error
+	err = ms.db.Debug().Find(&runModels).Where("pistage_run_id = ? ", pistageRunId).Error
 	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, err
 	}
